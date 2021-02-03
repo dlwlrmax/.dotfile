dbc4a7ba280077b0e512edb1c47ad8de08d9d6e5
Kien CT - F1 - D1 <kienct@fabbi.io>
2021-02-03 10:04:06 +0700

    xw

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ for i in "${arr[@]}"
 do
 	echo "remove ~/$i"
 	rm ~/"$i"
+	echo "link ~/.dotfile/"$i" ~/"$i""
    	ln -s ~/.dotfile/"$i" ~/"$i"
   	# or do whatever with individual element of the array
 done
