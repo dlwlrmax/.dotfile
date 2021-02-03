61c00a7a204d666940499873863cc10b9a3fe3b8
Kien CT - F1 - D1 <kienct@fabbi.io>
2021-02-03 12:04:09 +0700

    update

--- install.sh
+++ install.sh
@@ -7,11 +7,11 @@ if [ -d "$OHMYZSH" ]; then
 	rm -rf "$OHMYZSH"
 	echo "Remove $OHMYZSH"
 	echo "install oh-my-zsh"
-	echo "11234" | sudo -S -k sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+	echo "11234" | sudo -S -k sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 else 
 	echo "install oh-my-zsh"
-	echo "11234" | sudo -S -k sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+	echo "11234" | sudo -S -k sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
 
