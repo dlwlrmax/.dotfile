152b0d72b31e1ea372f9e9766aa03e940efac797
Kien CT - F1 - D1 <kienct@fabbi.io>
2021-02-03 15:21:17 +0700

    update

--- font.sh
+++ font.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pw="11234"
 echo "create font folder"
-echo $pw |sudo -S -k mkdir -p /usr/local/share/fonts/nerd-fonts
+echo $pw |sudo -S -k mkdir -p /usr/local/share/fonts/nerd-fonts/
 
-echo $pw |sudo -S -k cp ~/.dotfile/font/*.ttf /usr/local/share/fonts/nerd-fonts/
+echo $pw |sudo -S -k cp ~/.dotfile/font/ /usr/local/share/fonts/nerd-fonts/
