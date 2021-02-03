338995c27557aa1524a928ab78243dcd9b8a9024
Kien CT - F1 - D1 <kienct@fabbi.io>
2021-02-03 11:53:05 +0700

    update

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ OHMYZSH=~/.oh-my-zsh
 if [ -d "$OHMYZSH" ]; then
     echo "$OHMYZSH exists."
 	rm -rf "$OHMYZSH"
+	echo "Remove $OHMYZSH"
 	echo "install oh-my-zsh"
 	echo "11234" | sudo -S -k sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
