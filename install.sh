38e8440021d6f1ee591cc93347676ca8cad925dc
Kien CT - F1 - D1 <kienct@fabbi.io>
2021-02-03 11:45:39 +0700

    update

--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@ if [ -d "$OHMYZSH" ]; then
     echo "$OHMYZSH exists."
 else 
 	echo "install oh-my-zsh"
-	echo "11234" | sudo -S -k sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+	echo "11234" | sudo -S -k sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
 
 
 # echo $pw | sudo -S -k apt install tilix -y
 bash link.sh
+
+source ~/.zshrc
