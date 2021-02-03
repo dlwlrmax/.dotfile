c41a3ba6aea2b55ae6cc4a6b6649884491ea1dd8
Kien CT - F1 - D1 <kienct@fabbi.io>
2021-02-03 14:10:45 +0700

    update

--- install.sh
+++ install.sh
@@ -10,8 +10,15 @@ git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# echo $pw | sudo -S -k apt install tilix -y
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+echo $pw | sudo -S -k apt-get install software-properties-common
+echo $pw | sudo -S -k apt-add-repository -y ppa:rael-gc/rvm
+echo $pw | sudo -S -k apt-get update
+echo $pw | sudo -S -k apt-get install rvm
+echo $pw | sudo -S -k usermod -a -G rvm $USER
+rvm install ruby
+gem install colorls
+# echo $pw | sudo -S -k apt install ruby
 bash link.sh
 bash font.sh
 
