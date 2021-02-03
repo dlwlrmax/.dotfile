97873f652ee38545d8f7f121a6a1dfb0146625bd
Kien CT - F1 - D1 <kienct@fabbi.io>
2021-02-03 10:02:11 +0700

    add install

--- install.sh
+++ install.sh
@@ -6,13 +6,14 @@ OHMYZSH=~/.oh-my-zsh
 if [ -d "$OHMYZSH" ]; then
     echo "$OHMYZSH exists."
 else 
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	echo "install oh-my-zsh"
+	# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 # echo $pw | sudo -S -k apt install tilix -y
 for i in "${arr[@]}"
 do
-	# echo "remove ~/$i"
+	echo "remove ~/$i"
 	rm ~/"$i"
    	ln -s ~/.dotfile/"$i" ~/"$i"
   	# or do whatever with individual element of the array
