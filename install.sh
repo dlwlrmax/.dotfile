1ac64785ff51b887cc1b1821871ac2bb8dffe74c
Kien CT - F1 - D1 <kienct@fabbi.io>
2021-02-03 12:34:07 +0700

    update

--- install.sh
+++ install.sh
@@ -2,21 +2,17 @@
 # declare pw="11234"
 
 OHMYZSH=$HOME/.oh-my-zsh
-if [ -d "$OHMYZSH" ]; then
-    echo "$OHMYZSH exists."
-	rm -rf "$OHMYZSH"
-	echo "Remove $OHMYZSH"
-	echo "install oh-my-zsh"
-	echo "11234" | sudo -S -k sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
-	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-else 
-	echo "install oh-my-zsh"
-	echo "11234" | sudo -S -k sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
-	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-fi
+echo "$OHMYZSH exists."
+rm -rf "$OHMYZSH"
+echo "Remove $OHMYZSH"
+echo "install oh-my-zsh"
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 
 # echo $pw | sudo -S -k apt install tilix -y
 bash link.sh
 
 source ~/.zshrc
+
+chsh -s $(which zsh)
