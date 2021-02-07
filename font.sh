35943da656cd8ef25f0c6732e8df6788942d0437
Kien CT - F1 - D1 <kienct@fabbi.io>
2021-02-07 20:54:40 +0700

    ask pw

--- font.sh
+++ font.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo -n Password: 
+echo -n Enter your Password: 
 read -s pw
 echo "create font folder"
-echo $pw |sudo -S -k mkdir -p /usr/local/share/fonts/nerd-fonts/
+echo $pw |sudo -S -k mkdir -p ~/.local/fonts: 
 
 echo $pw |sudo -S -k cp ~/.dotfile/font/* /usr/local/share/fonts/nerd-fonts/
