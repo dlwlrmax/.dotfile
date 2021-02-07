f45fe6e5266f666be56cb5290013f763e2816f9d
Kien CT - F1 - D1 <kienct@fabbi.io>
2021-02-07 21:05:02 +0700

    dot

--- font.sh
+++ font.sh
@@ -2,6 +2,6 @@
 echo -n Enter your Password: 
 read -s pw
 echo "create font folder"
-echo $pw |sudo -S -k mkdir -p ~/.local/fonts: 
+echo $pw |sudo -S -k mkdir -p ~/.local/fonts 
 
 echo $pw |sudo -S -k cp ~/.dotfile/font/* ~/.local/fonts
